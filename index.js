a05884d27784d931ba5aab7fb1182ec1fc21529f
Adhish Raj <83546453+adhish09@users.noreply.github.com>
2023-03-18 13:51:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,13 +24,7 @@ const connect = () => {
     });
 };
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
 
 //middlewares
 app.use(cookieParser())
